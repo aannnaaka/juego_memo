cbab8c491fb321707f622c9f2a67727852efa1ed
aannnaaka <93240349+aannnaaka@users.noreply.github.com>
2023-06-16 16:02:39 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var turin
+
+function setup() {
+  createCanvas(1200,600);
+
+  turin = createSprite(600,300,20,50);
+ 
+}
+
+function draw() {
+  background("black");
+  
+  
+  drawSprites();
+}
+
